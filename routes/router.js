4e37f7bebf801f94d4e01515750f87fac23692fb
ridoy <ridoy51305@gmail.com>
2022-12-27 01:44:39 +0600

    changed some code to avoid path conflict in linux systems

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const apiRouter = require("./api/apiRouter");
 const router = express.Router();
-
+const { dirname } = require('path');
 // route for generating images from text
-router.use("/", express.static(require.main.path + "/public"));
+router.use("/", express.static(dirname(require.main.filename) + "/public"));
 router.use("/api/", apiRouter);
 
-// for 404 request
+// for 404 request 
 router.use((req, res) => {
   return res
     .status(404)
