cb73e7c07ec426256bd1371fe5ca9f76f4f4a0df
ridoy <ridoy51305@gmail.com>
2022-12-27 01:18:26 +0600

    changed folder structure

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const router = require("./api/router");
+const router = require("./routes/router");
 
 // environmental variables
 dotenv.config();
@@ -12,15 +12,7 @@ dotenv.config();
 app.use(bodyParser.json());
 
 // routes
-app.use("/", express.static("public"));
-app.use("/api", router);
-
-// for 404 request
-app.use((req, res) => {
-  return res
-    .status(404)
-    .json({ status: "fail", message: "resource not found" });
-});
+app.use(router);     
 
 // run app on available port
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
---
+++ routes/router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const apiRouter = require("./api/apiRouter");
+const router = express.Router();
+
+// route for generating images from text
+router.use("/", express.static('public'));
+router.use('/api/', apiRouter)
+ 
+// for 404 request
+router.use((req, res) => {
+  return res
+    .status(404)
+    .json({ status: "fail", message: "resource not found" });
+});
+
+module.exports = router;
