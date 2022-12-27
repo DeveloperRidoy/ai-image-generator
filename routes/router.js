6da05be0764f9900d1e47d5e3d05736034eb8d90
ridoy <ridoy51305@gmail.com>
2022-12-27 18:07:01 +0600

    used path module to serve static assets through express

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,12 @@ const express = require("express");
 const apiRouter = require("./api/apiRouter");
 const router = express.Router();
 const path = require("path");
+const { default: _default } = require("concurrently");
 
 // route for generating images from text
-router.use("/", express.static(require.main.path+'/public'));
+router.use("/", express.static(path.resolve(__dirname + "./../public")));
 router.use("/api/", apiRouter);
+
 
 // for 404 request
 router.use((req, res) => {
